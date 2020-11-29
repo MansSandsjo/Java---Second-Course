c6a91a5f4f6b7fed612776e58aaacd048347096b
Måns Sandsjö <75176496+MansSandsjo@users.noreply.github.com>
2020-11-29 13:06:46 +0100

    Add files via upload

---
+++ banktest/src/module-info.java
@@ -0,0 +1,17 @@
+module testett {
+	exports bst;
+	exports test;
+	exports queue_singlelinkedlist;
+	exports queue_delegate;
+	exports lpt;
+	exports bank;
+	exports mountain;
+	exports fractal;
+	exports drawing;
+	exports textproc;
+	exports testqueue;
+	exports koch;
+	exports map;
+
+	requires java.desktop;
+}
